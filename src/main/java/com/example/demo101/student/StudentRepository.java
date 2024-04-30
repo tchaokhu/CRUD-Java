33d45e454af7818c3f005008dfa7b27de281d07f
tchaokhu <nrnutthanapon@gmail.com>
2024-04-30 22:21:20 +0700

    fix something

--- src/main/java/com/example/demo101/student/StudentRepository.java
+++ src/main/java/com/example/demo101/student/StudentRepository.java
@@ -1,5 +1,6 @@
 package com.example.demo101.student;
 
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,7 +8,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface StudentRepository extends MongoRepository<Student, Long> {
+public interface StudentRepository extends MongoRepository<Student, UUID> {
     void deleteById(UUID id);
     Optional<Student> findStudentById(UUID id);
 
